51798966fa3c4627788b8c2e68cae10de9e3261d
Dawidek <dawidszpener141@gmail.com>
2020-04-17 15:25:58 +0100

    style: small style improvemnts and background picture

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import Layout from './Components/Layout/Layout'
 import DishCategory from './Components/DishCategory/DishCategory'
+import background from './background.png'
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+        <div className="bg-color">
+        <img src={background} alt="bg" className="bg"></img>
+      </div>
         <Layout>
           <DishCategory />
         </Layout>
