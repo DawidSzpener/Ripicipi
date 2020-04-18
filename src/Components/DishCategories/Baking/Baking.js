1513d7ccf74906dbbe8e46410349176a0cff4075
Dawidek <dawidszpener141@gmail.com>
2020-04-18 11:58:13 +0100

    feat: baking category can now dynamically render list of recipes in a nice card like style

--- src/Components/DishCategories/Baking/Baking.js
+++ src/Components/DishCategories/Baking/Baking.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Recipe from '../../Recipe/Recipe'
 import picture from '../../../assets/pictures/background2.png'
+import Aux from '../../../hoc/Aux'
+import './Baking.css'
 
-const baking = () => {
-  const recipeList = [
-    <Recipe title='Testowy tytul' picture={picture}/>,
-    <Recipe title='Testowy tytul' picture={picture}/>
-  ]
-  return (
-    <div className='Baking'>
-      <div className='RecipeList'>
-        {recipeList}
+class Baking extends Component {
+  state = {
+    recipeList: [
+      <Recipe title='Testowy tytul' picture={picture}/>,
+      <Recipe title='Testowy tytul' picture={picture}/>,
+      <Recipe title='Testowy tytul' picture={picture}/>,
+      <Recipe title='Testowy tytul' picture={picture}/>,
+      <Recipe title='Testowy tytul' picture={picture}/>
+    ]
+  }
+
+  render() {
+
+    const squashingRecipes = 
+      this.state.recipeList.map(recipe => {
+        return (
+          <Aux>
+            <div className='SingleRecipeCard'>
+              <div className='SingleRecipeCardTitle'>{recipe.props.title}</div>
+              <img src={recipe.props.picture} alt="card_bg"></img>
+            </div>
+          </Aux>
+        )
+      })
+
+
+    return (
+      <div className='Baking'>
+        {squashingRecipes}
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default baking
+export default Baking
