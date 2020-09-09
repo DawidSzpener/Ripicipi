a11e4acc498c620e7a744bcf1100db71619cb073
Dawid Szpener <dawidszpener141@gmail.com>
2020-09-09 17:11:04 +0100

    chore: adjusted commandments style

--- src/Containers/WelcomeScreen/WelcomeScreen.js
+++ src/Containers/WelcomeScreen/WelcomeScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Aux from '../../hoc/Aux'
 import './WelcomeScreen.css'
 import background from '../../assets/pictures/background7.png'
+import comandements from '../../assets/pictures/commandments.png'
 
 class Homescreen extends Component {
   goToCategories = () => {
@@ -18,14 +19,7 @@ class Homescreen extends Component {
         <div className="Background">
           <img src={background} alt="bg" className="bg-homescreen"></img>
           <div className='Homescreen-buttons'>
-            <h3>Read the Recipe Before Beginning</h3>
-            <h3>Weigh Your Ingredients Properly</h3>
-            <h3>Always Have Ingredients Prepped</h3>
-            <h3>Follow the Recipe</h3>
-            <h3>Don’t Over-mix, Don’t Under-mix</h3>
-            <h3>Don’t Open the Oven</h3>
-            <h3>Allow to Cool Completely</h3>
-            <h3>Store Properly</h3>
+            <img src={comandements} alt="bg" className="comandements"></img>
           </div>
         </div>
       </Aux>
