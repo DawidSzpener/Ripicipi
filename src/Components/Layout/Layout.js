cc9fa12317e05200e073e3c96cad7fa0c570a30f
Dawid Szpener <dawidszpener141@gmail.com>
2021-02-24 14:39:21 +0000

    Receive all recipes properties in order to implement search functionality

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 import Aux from '../../hoc/Aux'
+import axios from '../../axios-recipes'
 import './Layout.css'
 
 class Layout extends Component {
   state = {
     showSideDrawer: false
+  }
+
+  componentDidMount() {
+    axios.get(`/recipes.json`)
+    .then(res => {
+      const data = Object.values(res.data)
+      data.map(category => {
+        Object.values(category).map(recipe => {
+          console.log(recipe.title)
+        })
+      })
+    })
   }
 
   sideDrawerClosedHandler = () => {
