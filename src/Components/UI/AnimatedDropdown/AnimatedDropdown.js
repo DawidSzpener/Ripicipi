21875ef1013c0a2627c503c0b40a47463cec51b7
Dawid Szpener <dawidszpener141@gmail.com>
2021-03-01 11:21:38 +0000

    show categories on hover instead on click in the navbar

--- src/Components/UI/AnimatedDropdown/AnimatedDropdown.js
+++ src/Components/UI/AnimatedDropdown/AnimatedDropdown.js
@@ -5,16 +5,10 @@ import './AnimatedDropdown.scss'
 
 const AnimatedDropdown = (props) => {
   const [list, setList] = useState(props.list)
-  const [showList, setShowList] = useState(false)
   const [selectedItem, setSelectedItem] = useState(null)
-
-  function showDropDown() {
-    setShowList(!showList)
-  }
 
   function selectItem(item) {
     setSelectedItem(item)
-    setShowList(false)
     props.history.push(`/categories/${item.value}`)
     window.location.reload()
   }
@@ -39,12 +33,12 @@ const AnimatedDropdown = (props) => {
 
   if(props.shape === 'circle') {
     header = 
-    <div className="AnimatedDropdown__header" onClick={() => showDropDown()}>
+    <div className="AnimatedDropdown__header">
       <img className={shape + "--title"} src={props.picture} alt="foto"/>
     </div>
   } else {
     header =
-    <div className="AnimatedDropdown__header" onClick={() => showDropDown()}>
+    <div className="AnimatedDropdown__header">
       <div className={shape + "--title"} >{props.title}</div>
     </div>
   }
@@ -59,7 +53,7 @@ const AnimatedDropdown = (props) => {
       {header}
       <div
         className={props.shape === "circle" ? 'AnimatedDropdown__list AnimatedDropdown__list--circle' : 'AnimatedDropdown__list AnimatedDropdown__list--rectangle'+ headerPosition}
-        style={{ display: showList ? "block" : "none" }}>
+        >
         {list.map(item => (
           <div className={"AnimatedDropdown__item AnimatedDropdown__item--" + theme} key={item.id} onClick={() => selectItem(item)}>
             <div style={{paddingTop: "12px"}}>{item.value[0].toUpperCase() + item.value.slice(1)}</div>
